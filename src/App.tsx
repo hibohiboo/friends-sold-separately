9e5be21a3ce5687af9fd96d7ec8f80f3abe4cf43
hibohiboo <swdice66@gmail.com>
2022-11-05 23:50:54 +0900

    fix tsconfig一本化 AB#214

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-const App = () => {
+const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
