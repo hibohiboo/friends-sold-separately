87d932d2bb6a32b877a0a18dc249df81f48fbafa
hibohiboo <swdice66@gmail.com>
2022-11-06 01:38:53 +0900

    fix cdk 追加 AB#214

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import * as dotenv from 'dotenv';
+import AWSFriendsSoldSeparatelytack from '../lib/cdk-stack';
+
+dotenv.config();
+const envList = [
+  'PROJECT_ID',
+  'BUCKET_NAME',
+  'DISTRIBUTION_ID',
+  'TAG_PROJECT_NAME',
+  'DOMAIN_NAME',
+] as const;
+// eslint-disable-next-line no-restricted-syntax
+for (const key of envList) {
+  if (!process.env[key]) throw new Error(`please add ${key} to .env`);
+}
+const processEnv = process.env as Record<typeof envList[number], string>;
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+const projectId = processEnv.PROJECT_ID;
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// eslint-disable-next-line no-new
+new AWSFriendsSoldSeparatelytack(app, `${projectId}-stack`, {
+  bucketName: processEnv.BUCKET_NAME,
+  projectId: `${projectId}`,
+  distributionId: processEnv.DISTRIBUTION_ID,
+  domainName: processEnv.DOMAIN_NAME,
+  projectNameTag: processEnv.TAG_PROJECT_NAME,
+  env,
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,77 @@
-import * as cdk from 'aws-cdk-lib';
+import * as core from 'aws-cdk-lib';
+import * as cf from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { basePath } from '../constants/paths';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface Props extends core.StackProps {
+  bucketName: string;
+  distributionId: string;
+  domainName: string;
+  projectNameTag: string;
+  projectId: string;
+}
+
+const createPolicy = (bucket: s3.IBucket, identity: cf.OriginAccessIdentity) => {
+  const myBucketPolicy = new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    actions: ['s3:GetObject', 's3:ListBucket'],
+    principals: [
+      new iam.CanonicalUserPrincipal(identity.cloudFrontOriginAccessIdentityS3CanonicalUserId),
+    ],
+    resources: [`${bucket.bucketArn}/*`, bucket.bucketArn],
+  });
+  return myBucketPolicy;
+};
+class AWSFriendsSoldSeparatelytack extends core.Stack {
+  constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
+    // CloudFront オリジン用のS3バケットへの参照を取得
+    const bucket = s3.Bucket.fromBucketName(this, props.bucketName, props.bucketName);
+    // CloudFront で設定する オリジンアクセスアイデンティティ を作成
+    const identity = this.createIdentity(
+      bucket,
+      `${props.projectId}-origin-access-identity-to-s3-bucket`
+    );
+    // S3バケットポリシーで、CloudFrontのオリジンアクセスアイデンティティを許可
+    bucket.addToResourcePolicy(createPolicy(bucket, identity));
 
-    // The code that defines your stack goes here
+    // CloudFrontディストリビューションを作成
+    const distribution = cf.Distribution.fromDistributionAttributes(this, props.distributionId, {
+      domainName: '',
+      distributionId: props.distributionId,
+    });
+    // 指定したディレクトリをデプロイ
+    this.deployS3(bucket, distribution, '../dist', props.bucketName);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    core.Tags.of(this).add('Project', props.projectNameTag);
+  }
+
+  private createIdentity(bucket: s3.IBucket, identityName: string) {
+    const identity = new cf.OriginAccessIdentity(this, identityName, {
+      comment: `${bucket.bucketName} access identity`,
+    });
+    return identity;
+  }
+
+  private deployS3(
+    siteBucket: s3.IBucket,
+    distribution: cf.IDistribution,
+    sourcePath: string,
+    bucketName: string
+  ) {
+    // Deploy site contents to S3 bucket
+    // eslint-disable-next-line no-new
+    new s3deploy.BucketDeployment(this, `${bucketName}-deploy-with-invalidation`, {
+      sources: [s3deploy.Source.asset(sourcePath)],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: [`/${basePath}/*`],
+      destinationKeyPrefix: basePath,
+    });
   }
 }
+
+export default AWSFriendsSoldSeparatelytack;
