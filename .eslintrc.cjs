aae979926dcd94f4e97b06d9a00a5cc9ce56d5f2
hibohiboo <swdice66@gmail.com>
2022-11-06 18:38:16 +0900

    fix eslint import AB#219

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    es6: true,
   },
   extends: [
     'airbnb',
@@ -22,7 +21,15 @@ module.exports = {
   },
   overrides: [],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier', 'testing-library', 'jest-dom'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'testing-library',
+    'jest-dom',
+    'unused-imports',
+    'import',
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
@@ -30,5 +37,56 @@ module.exports = {
     // react-redux
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
+    // import部分
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        // グループごとの並び順
+        groups: [
+          'builtin', // 1. fsや path などの node "builtin" のモジュール
+          'external', // 2. npm install したパッケージ
+          'internal', // 3. webpack などでパス設定したモジュール
+          ['parent', 'sibling'], // 4. 親階層と小階層のファイル
+          'object', // object"-imports
+          'type', // 型だけをインポートする type imports
+          'index', // 同階層のファイル
+        ],
+        // グループごとに改行を入れる
+        'newlines-between': 'never', // "never" を指定すると改行なし
+        pathGroupsExcludedImportTypes: ['builtin'],
+        // アルファベット順・大文字小文字を区別しない
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          // react 関連を external より前にする
+          {
+            pattern: 'react**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '{@/store/**,@/domain/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          // `@/components` の import をグルーピング
+          {
+            pattern: '{@/components/**,./**.style}',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+      },
+    ],
   },
 };
