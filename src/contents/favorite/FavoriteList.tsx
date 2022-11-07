973373181c24b0aebf31245f07134e5435c3ecb3
hibohiboo <swdice66@gmail.com>
2022-11-08 07:32:27 +0900

    fix 分かりにくい文言修正 AB#223

--- src/contents/favorite/FavoriteList.tsx
+++ src/contents/favorite/FavoriteList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdNotifications } from 'react-icons/md';
 import { useAppSelector } from '@/store/hooks';
 import { favoriteEntitySelector } from '@/store/selectors/favoriteSelector';
 
@@ -6,10 +7,13 @@ const FavoriteList: React.FC = () => {
   const favorites = useAppSelector(favoriteEntitySelector);
   return (
     <div style={{ padding: '0 10px', backgroundColor: '#111' }}>
-      <h2>握手履歴</h2>
+      <h2 style={{ display: 'flex', paddingTop: '1.5rem' }}>
+        <MdNotifications />
+        <div style={{ marginLeft: '1rem' }}>僕も私も</div>
+      </h2>
       <div>
         {favorites.map((fav) => (
-          <div key={fav.id}>
+          <div key={fav.id} style={{ marginBottom: '10px' }}>
             {fav.friendTwitterId ? (
               <a
                 href={`https://twitter.com/${fav.friendTwitterId}`}
