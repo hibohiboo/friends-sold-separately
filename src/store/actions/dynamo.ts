3fad3b684e499a3d8d725c4d2fbfd097ec4e1518
hibohiboo <swdice66@gmail.com>
2022-11-13 13:28:29 +0900

    fix 初期状態修正 AB#233

--- src/store/actions/dynamo.ts
+++ src/store/actions/dynamo.ts
@@ -1,17 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { UUID } from '@/domain/udonarium/class/core/system/util/uuid';
 import { getUser } from '@/domain/udonarium/room/lobby';
 import { getUsers, putUser } from '@/domain/user/api';
 import { toPutUserContext } from '@/domain/user/converter';
 import { RootState } from '..';
+import { userProfileSlice } from '../slices/userProfile';
 import { usersSlice } from '../slices/users';
 
 export const connectServer = createAsyncThunk<void, void, { state: RootState }>(
   'connectServer',
   async (req, thunkAPI) => {
     const state = thunkAPI.getState();
-    console.log('get user');
+    if (!state.userProfile.identifier) {
+      const id = UUID.generateUuid();
+      console.log('new id', id);
+      thunkAPI.dispatch(
+        userProfileSlice.actions.setProfile({
+          identifier: id,
+          userId: id,
+          peerId: '',
+          name: 'ミエナイトモダチ',
+          twitterId: '',
+          isPublish: false,
+          favoriteAttribute: [],
+        })
+      );
+    }
     const users = await getUsers();
-    console.log('connect users', users);
 
     thunkAPI.dispatch(usersSlice.actions.setUsersContext(users));
   }
